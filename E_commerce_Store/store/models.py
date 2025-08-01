a6f071a220613a7734db1d4ec34f4737f0cef7ac
scorpion <scorpion@shadow.sccorpion>
2025-08-02 02:40:34 +0500

    add product and category model and list the product on the page

---
+++ E_commerce_Store/store/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+
+from django.utils.text import slugify
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    def __str__(self):
+            return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug or Category.objects.filter(slug=self.slug).exclude(pk =self.pk).exists():
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+            while Category.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+    
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug or Product.objects.filter(slug=self.slug).exclude(pk=self.pk).exists():
+            base_slug = slugify(self.name)
+            if self.category:
+                base_slug = f"{base_slug}-{self.category.slug}"
+            
+            slug = base_slug
+            counter =1
+            while Product.objects.filter(slug=slug).exclude(pk=self.pk).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+            
+# Cart Model
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def subtotal(self):
+        return self.product.price * self.quantity
+    
